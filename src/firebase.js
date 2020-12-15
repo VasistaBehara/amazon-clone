9297ef14de6d28b3461d9eefc6170b3a4dd33eca
vasistabehara <bvavinash007@gmail.com>
2020-12-15 13:15:02 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIhc6SThrOmSxdcWx1O8uy0cot9EerGHw",
+    authDomain: "clone-f917f.firebaseapp.com",
+    projectId: "clone-f917f",
+    storageBucket: "clone-f917f.appspot.com",
+    messagingSenderId: "19457730679",
+    appId: "1:19457730679:web:00c2f54e5c6b35e1defe72",
+    measurementId: "G-RYQR7LP6PV"
+  };
