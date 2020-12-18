f067e1494146e4f45fbdb7aaddde39306e301012
vasistabehara <bvavinash007@gmail.com>
2020-12-17 23:40:53 -0500

    dec17_1140

--- src/Product.js
+++ src/Product.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import './Product.css'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import { useStateValue } from './StateProvider';
 
 function Product({ id, title, image, price, rating }) {
+
+    const [ state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+             type: "ADD_TO_BASKET",
+             item: {
+                 id: id,
+                 title: title,
+                 image: image,
+                 price: price,
+                 rating: rating,
+             },
+        });
+    };
+    
     return (
         <div className="product">
             <div className="product_info">
@@ -19,7 +36,7 @@ function Product({ id, title, image, price, rating }) {
             </div>
                 <img src= {image}
                 alt={title} />
-            <button>
+            <button onClick={addToBasket}>
                 <ShoppingCartIcon/>
             </button>
         </div>
